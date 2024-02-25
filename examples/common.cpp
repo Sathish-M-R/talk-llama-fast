ca8d87984eeb5f173e8a5d4fd074d46200bf6a36
Mozer <mo3rnt@gmail.com>
2024-02-25 14:55:23 +0300

    Add files via upload

--- examples/common.cpp
+++ examples/common.cpp
@@ -732,8 +732,46 @@ void high_pass_filter(std::vector<float> & data, float cutoff, float sample_rate
     }
 }
 
+bool vad_simple(std::vector<float> & pcmf32, int sample_rate, int last_ms, float vad_thold, float freq_thold, bool verbose) {
+    const int n_samples      = pcmf32.size();
+    const int n_samples_last = (sample_rate * last_ms) / 1000;
+
+    if (n_samples_last >= n_samples) {
+        // not enough samples - assume no speech
+        return false;
+    }
+
+    if (freq_thold > 0.0f) {
+        high_pass_filter(pcmf32, freq_thold, sample_rate);
+    }
+
+    float energy_all  = 0.0f;
+    float energy_last = 0.0f;
+
+    for (int i = 0; i < n_samples; i++) {
+        energy_all += fabsf(pcmf32[i]);
+
+        if (i >= n_samples - n_samples_last) {
+            energy_last += fabsf(pcmf32[i]);
+        }
+    }
+
+    energy_all  /= n_samples;
+    energy_last /= n_samples_last;
+
+    if (verbose) {
+        fprintf(stderr, "%s: energy_all: %f, energy_last: %f, vad_thold: %f, freq_thold: %f\n", __func__, energy_all, energy_last, vad_thold, freq_thold);
+    }
+
+    if (energy_last > vad_thold*energy_all) {
+        return false;
+    }
+
+    return true;
+}
+
 // returns: 0:no speech, 1:speech_started, 2:speech_ended
-int vad_simple(std::vector<float> & pcmf32, int sample_rate, int last_ms, float vad_thold, float freq_thold, bool verbose) {
+int vad_simple_int(std::vector<float> & pcmf32, int sample_rate, int last_ms, float vad_thold, float freq_thold, bool verbose, float vad_start_thold=0.000270f) {
     const int n_samples      = pcmf32.size();
     const int n_samples_last = (sample_rate * last_ms) / 1000;
 
@@ -765,9 +803,9 @@ int vad_simple(std::vector<float> & pcmf32, int sample_rate, int last_ms, float 
     }
 
     if (energy_last > vad_thold*energy_all) {
-		if (energy_last > 0.000270) 
+		if (vad_start_thold && energy_last > vad_start_thold) 
 		{
-			if (verbose) printf("speech_start: energy_last > 0.000270\n");
+			if (verbose) printf("[speech_start!]: energy_last > %f\n", vad_start_thold);
 			return 1;
 		}
         return 0;
